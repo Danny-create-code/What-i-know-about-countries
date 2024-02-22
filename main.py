cc685f33c858bde7f4bd0fdb284f320bd6480640
Danny-create-code <da.svacek@gmail.com>
2024-02-22 12:20:49 +0100

    I created window and loop

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+from tkinter import *
 import requests
 
+# imported data
 url = "https://restcountries.com/v3.1/all"
 headers = {"accept": "application/json"}
 response = requests.get(url, headers=headers)
 data = response.json()
 
+# window setting
+window = Tk()
+window.minsize(640, 480)
+window.resizable(False, False)
+window.title('countries')
+
+# main loop
+window.mainloop()
