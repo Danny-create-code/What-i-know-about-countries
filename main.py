191ef967dfc81831504d914c8dd8d9a3841329b7
Danny-create-code <da.svacek@gmail.com>
2024-02-22 13:12:12 +0100

    I have obtained the data necessary for the application

--- main.py
+++ main.py
@@ -13,5 +13,24 @@ window.minsize(640, 480)
 window.resizable(False, False)
 window.title('countries')
 
+# information obtained
+countries = []
+timezones = []
+populations = []
+regions = []
+maps = []
+flags = []
+flags_text = []
+
+for x in range(0, len(data)):
+    countries.append(data[x]["name"]["official"])
+    timezones.append(data[x]["timezones"])
+    populations.append(data[x]["population"])
+    regions.append(data[x]["region"])
+    maps.append(data[x]["maps"]["googleMaps"])
+    flags.append(data[x]["flags"]["png"])
+
+
 # main loop
 window.mainloop()
+
